82e11386d442485c771734cf3a22809e456b68bf
emiraydgn <emiraydogan03@gmail.com>
2026-03-25 12:54:36 +0300

    some fixes at stack

--- aydogan.emir/S2/stack.hpp
+++ aydogan.emir/S2/stack.hpp
@@ -2,7 +2,8 @@
 #define STACK_HPP
 
 #include "list.hpp"
-#include "stdexcept"
+#include <stdexcept>
+#include <utility>
 
 namespace aydogan
 {
@@ -11,47 +12,57 @@ namespace aydogan
   {
   public:
     Stack() = default;
+    Stack(const Stack&) = default;
+    Stack(Stack&&) = default;
+    Stack& operator=(const Stack&) = default;
+    Stack& operator=(Stack&&) = default;
+    ~Stack() = default;
+
     bool empty() const noexcept
-{
-  return data_.empty();
-}
+    {
+      return data_.empty();
+    }
 
-void push(const T& rhs)
-{
-    data_.push_front(rhs);
-}
-void push(T&& rhs)
-{
-    data_.push_front(std::move(rhs));
-}
-T& top()
-{
-  if (empty())
-  {
-    throw std::out_of_range("Stack is empty");
-  }
-  return data_.front();
-}
+    void push(const T& rhs)
+    {
+      data_.push_front(rhs);
+    }
 
-const T& top() const
-{
-  if (empty())
-  {
-    throw std::out_of_range("Stack is empty");
-  }
-  return data_.front();
-}
-T drop()
-{
-  if (empty())
-  {
-    throw std::out_of_range("Stack is empty");
-  }
+    void push(T&& rhs)
+    {
+      data_.push_front(std::move(rhs));
+    }
 
-  T value = std::move(data_.front());
-  data_.pop_front();
-  return value;
-}
+    T& top()
+    {
+      if (empty())
+      {
+        throw std::out_of_range("Stack is empty");
+      }
+      return data_.front();
+    }
+
+    const T& top() const
+    {
+      if (empty())
+      {
+        throw std::out_of_range("Stack is empty");
+      }
+      return data_.front();
+    }
+
+    T drop()
+    {
+      if (empty())
+      {
+        throw std::out_of_range("Stack is empty");
+      }
+
+      T value = std::move(data_.front());
+      data_.pop_front();
+      return value;
+    }
+
   private:
     List< T > data_;
   };
