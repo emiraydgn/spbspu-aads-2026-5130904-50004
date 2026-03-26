06af4f8c9b4e13a34e4da6036d81715270a45be6
emiraydgn <emiraydogan03@gmail.com>
2026-03-26 21:31:30 +0300

    final fix

--- aydogan.emir/S2/list.hpp
+++ aydogan.emir/S2/list.hpp
@@ -10,9 +10,41 @@ namespace aydogan
   class List;
 
   template< class T >
+  class Iterator;
+
+  template< class T >
+  class ConstIterator;
+
+  namespace detail
+  {
+    template< class T >
+    struct Node
+    {
+      T data;
+      Node* next;
+
+      Node():
+        data(),
+        next(nullptr)
+      {}
+
+      Node(const T& value, Node* nextNode):
+        data(value),
+        next(nextNode)
+      {}
+
+      Node(T&& value, Node* nextNode):
+        data(std::move(value)),
+        next(nextNode)
+      {}
+    };
+  }
+
+  template< class T >
   class Iterator
   {
     friend class List< T >;
+    friend class ConstIterator< T >;
 
   public:
     Iterator():
@@ -24,10 +56,25 @@ namespace aydogan
       return node_->data;
     }
 
+    T* operator->() const
+    {
+      return &(node_->data);
+    }
+
     Iterator& operator++()
     {
-      node_ = node_->next;
+      if (node_ != nullptr)
+      {
+        node_ = node_->next;
+      }
       return *this;
+    }
+
+    Iterator operator++(int)
+    {
+      Iterator tmp(*this);
+      ++(*this);
+      return tmp;
     }
 
     bool operator==(const Iterator& other) const
@@ -41,38 +88,129 @@ namespace aydogan
     }
 
   private:
-    struct Node
-    {
-      T data;
-      Node* next;
-
-      Node(const T& value, Node* nextNode):
-        data(value),
-        next(nextNode)
-      {}
-    };
-
-    explicit Iterator(Node* node):
+    explicit Iterator(detail::Node< T >* node):
       node_(node)
     {}
 
-    Node* node_;
+    detail::Node< T >* node_;
+  };
+
+  template< class T >
+  class ConstIterator
+  {
+    friend class List< T >;
+
+  public:
+    ConstIterator():
+      node_(nullptr)
+    {}
+
+    ConstIterator(const Iterator< T >& other):
+      node_(other.node_)
+    {}
+
+    const T& operator*() const
+    {
+      return node_->data;
+    }
+
+    const T* operator->() const
+    {
+      return &(node_->data);
+    }
+
+    ConstIterator& operator++()
+    {
+      if (node_ != nullptr)
+      {
+        node_ = node_->next;
+      }
+      return *this;
+    }
+
+    ConstIterator operator++(int)
+    {
+      ConstIterator tmp(*this);
+      ++(*this);
+      return tmp;
+    }
+
+    bool operator==(const ConstIterator& other) const
+    {
+      return node_ == other.node_;
+    }
+
+    bool operator!=(const ConstIterator& other) const
+    {
+      return node_ != other.node_;
+    }
+
+  private:
+    explicit ConstIterator(const detail::Node< T >* node):
+      node_(node)
+    {}
+
+    const detail::Node< T >* node_;
   };
 
   template< class T >
   class List
   {
-    using Node = typename Iterator< T >::Node;
+    friend class Iterator< T >;
+    friend class ConstIterator< T >;
 
   public:
     List():
-      fake_(new Node(T{}, nullptr))
+      fake_(new detail::Node< T >())
     {}
+
+    List(const List& other):
+      List()
+    {
+      Iterator< T > tail = beforeBegin();
+      for (ConstIterator< T > it = other.cbegin(); it != other.cend(); ++it)
+      {
+        tail = insertAfter(tail, *it);
+      }
+    }
+
+    List(List&& other):
+      fake_(other.fake_)
+    {
+      other.fake_ = new detail::Node< T >();
+    }
 
     ~List()
     {
       clear();
       delete fake_;
+    }
+
+    List& operator=(const List& other)
+    {
+      if (this != &other)
+      {
+        List tmp(other);
+        swap(tmp);
+      }
+      return *this;
+    }
+
+    List& operator=(List&& other)
+    {
+      if (this != &other)
+      {
+        clear();
+        delete fake_;
+        fake_ = other.fake_;
+        other.fake_ = new detail::Node< T >();
+      }
+      return *this;
+    }
+
+    void swap(List& other) noexcept
+    {
+      std::swap(fake_, other.fake_);
     }
 
     bool empty() const noexcept
@@ -85,14 +223,39 @@ namespace aydogan
       return Iterator< T >(fake_);
     }
 
+    ConstIterator< T > beforeBegin() const noexcept
+    {
+      return ConstIterator< T >(fake_);
+    }
+
     Iterator< T > begin() noexcept
     {
       return Iterator< T >(fake_->next);
     }
 
+    ConstIterator< T > begin() const noexcept
+    {
+      return ConstIterator< T >(fake_->next);
+    }
+
+    ConstIterator< T > cbegin() const noexcept
+    {
+      return ConstIterator< T >(fake_->next);
+    }
+
     Iterator< T > end() noexcept
     {
       return Iterator< T >(nullptr);
+}
+
+    ConstIterator< T > end() const noexcept
+    {
+      return ConstIterator< T >(nullptr);
+    }
+
+    ConstIterator< T > cend() const noexcept
+    {
+      return ConstIterator< T >(nullptr);
     }
 
     T& front()
@@ -104,9 +267,23 @@ namespace aydogan
       return fake_->next->data;
     }
 
+    const T& front() const
+    {
+      if (empty())
+      {
+        throw std::out_of_range("List is empty");
+      }
+      return fake_->next->data;
+    }
+
     void push_front(const T& value)
     {
       insertAfter(beforeBegin(), value);
+    }
+
+    void push_front(T&& value)
+    {
+      insertAfter(beforeBegin(), std::move(value));
     }
 
     void pop_front()
@@ -125,9 +302,24 @@ namespace aydogan
         throw std::out_of_range("Invalid iterator");
       }
 
-      Node* newNode = new Node(value, pos.node_->next);
-      pos.node_->next = newNode;
+      detail::Node< T >* newNode =
+        new detail::Node< T >(value, pos.node_->next);
 
+      pos.node_->next = newNode;
+      return Iterator< T >(newNode);
+    }
+
+    Iterator< T > insertAfter(Iterator< T > pos, T&& value)
+    {
+      if (pos.node_ == nullptr)
+      {
+        throw std::out_of_range("Invalid iterator");
+      }
+
+      detail::Node< T >* newNode =
+        new detail::Node< T >(std::move(value), pos.node_->next);
+
+      pos.node_->next = newNode;
       return Iterator< T >(newNode);
     }
 
@@ -138,7 +330,7 @@ namespace aydogan
         throw std::out_of_range("Nothing to erase");
       }
 
-      Node* toDelete = pos.node_->next;
+      detail::Node< T >* toDelete = pos.node_->next;
       pos.node_->next = toDelete->next;
       delete toDelete;
 
@@ -149,14 +341,14 @@ namespace aydogan
     {
       while (!empty())
       {
-        Node* tmp = fake_->next;
+        detail::Node< T >* tmp = fake_->next;
         fake_->next = tmp->next;
         delete tmp;
       }
     }
 
   private:
-    Node* fake_;
+    detail::Node< T >* fake_;
   };
 }
 
