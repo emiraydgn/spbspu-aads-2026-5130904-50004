ba5b316c260c2009c32fe28e1c59d2f18d8f70a0
emiraydgn <emiraydogan03@gmail.com>
2026-05-21 11:23:08 +0300

    load graphs and run command loop

--- aydogan.emir/S3/command_processor.cpp
+++ aydogan.emir/S3/command_processor.cpp
@@ -25,3 +25,75 @@ void aydogan::CommandProcessor::registerCommands()
   commandTable_.insert("merge", &CommandProcessor::handleMerge);
   commandTable_.insert("extract", &CommandProcessor::handleExtract);
 }
+
+void aydogan::CommandProcessor::loadGraphs(const std::string& filename)
+{
+  std::ifstream input(filename);
+
+  if (!input)
+  {
+    throw std::runtime_error("Cannot open file");
+  }
+
+  std::string line;
+
+  while (std::getline(input, line))
+  {
+    Tokens header = tokenize(line);
+
+    if (header.empty())
+    {
+      continue;
+    }
+
+    if (header.size() != 2)
+    {
+      continue;
+    }
+
+    unsigned int edgeCount = 0;
+
+    if (!parseUnsigned(header[1], edgeCount))
+    {
+      continue;
+    }
+
+    Graph graph;
+
+    for (unsigned int i = 0; i < edgeCount; ++i)
+    {
+      std::string edgeLine;
+
+      if (!std::getline(input, edgeLine))
+      {
+        break;
+      }
+
+      Tokens edgeTokens = tokenize(edgeLine);
+
+      if (edgeTokens.size() != 3)
+      {
+        continue;
+      }
+
+      unsigned int weight = 0;
+
+      if (parseUnsigned(edgeTokens[2], weight))
+      {
+        graph.addEdge(edgeTokens[0], edgeTokens[1], weight);
+      }
+    }
+
+    graphStorage_.insert(header[0], graph);
+  }
+}
+
+void aydogan::CommandProcessor::run(std::istream& input, std::ostream& output)
+{
+  std::string line;
+
+  while (std::getline(input, line))
+  {
+    processLine(line, output);
+  }
+}
