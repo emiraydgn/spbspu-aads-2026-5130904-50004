622ef1c622f1fc428db378c66420f9980170611e
emiraydgn <emiraydogan03@gmail.com>
2026-05-21 11:37:13 +0300

    add command parsing helpers

--- aydogan.emir/S3/command_processor.cpp
+++ aydogan.emir/S3/command_processor.cpp
@@ -97,3 +97,131 @@ void aydogan::CommandProcessor::run(std::istream& input, std::ostream& output)
     processLine(line, output);
   }
 }
+
+void aydogan::CommandProcessor::processLine(const std::string& line, std::ostream& output)
+{
+  Tokens tokens = tokenize(line);
+
+  if (tokens.empty())
+  {
+    return;
+  }
+
+  Handler* handler = commandTable_.find(tokens[0]);
+
+  if (handler == nullptr)
+  {
+    printInvalid(output);
+    return;
+  }
+
+  (*handler)(*this, tokens, output);
+}
+
+aydogan::CommandProcessor::Tokens aydogan::CommandProcessor::tokenize(const std::string& line)
+{
+  Tokens tokens;
+  std::string current;
+
+  for (char symbol: line)
+  {
+    if (std::isspace(static_cast< unsigned char >(symbol)))
+    {
+      if (!current.empty())
+      {
+        tokens.push_back(current);
+        current.clear();
+      }
+    }
+    else
+    {
+      current += symbol;
+    }
+  }
+
+  if (!current.empty())
+  {
+    tokens.push_back(current);
+  }
+
+  return tokens;
+}
+
+bool aydogan::CommandProcessor::parseUnsigned(const std::string& text, unsigned int& value)
+{
+  if (text.empty())
+  {
+    return false;
+  }
+
+  unsigned long long result = 0;
+
+  for (char symbol: text)
+  {
+    if (!std::isdigit(static_cast< unsigned char >(symbol)))
+    {
+      return false;
+    }
+
+    result = result * 10 + static_cast< unsigned int >(symbol - '0');
+
+    if (result > std::numeric_limits< unsigned int >::max())
+    {
+      return false;
+    }
+  }
+
+  value = static_cast< unsigned int >(result);
+  return true;
+}
+
+bool aydogan::CommandProcessor::hasDuplicates(const std::vector< std::string >& values)
+{
+  std::vector< std::string > copy(values);
+  std::sort(copy.begin(), copy.end());
+
+  for (std::size_t i = 1; i < copy.size(); ++i)
+  {
+    if (copy[i] == copy[i - 1])
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void aydogan::CommandProcessor::printInvalid(std::ostream& output)
+{
+  output << "<INVALID COMMAND>\n";
+}
+
+void aydogan::CommandProcessor::printEdges(
+  const std::vector< EdgeInfo >& edges,
+  std::ostream& output
+)
+{
+  if (edges.empty())
+  {
+    output << "\n";
+    return;
+  }
+
+  std::string currentVertex = edges[0].vertex;
+  output << currentVertex << " " << edges[0].weight;
+
+  for (std::size_t i = 1; i < edges.size(); ++i)
+  {
+    if (edges[i].vertex == currentVertex)
+    {
+      output << " " << edges[i].weight;
+    }
+    else
+    {
+      currentVertex = edges[i].vertex;
+      output << "\n" << currentVertex << " " << edges[i].weight;
+    }
+  }
+
+  output << "\n";
+}
