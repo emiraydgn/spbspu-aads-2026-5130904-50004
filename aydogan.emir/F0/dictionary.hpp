80998cad352c4dbc811e6d01c78a5c650c054234
emiraydgn <emiraydogan03@gmail.com>
2026-06-29 20:17:57 +0300

    replace dictionary storage map with list storage

--- aydogan.emir/F0/dictionary.hpp
+++ aydogan.emir/F0/dictionary.hpp
@@ -1,16 +1,119 @@
 #ifndef DICTIONARY_HPP
 #define DICTIONARY_HPP
 
+#include <cstddef>
 #include <iosfwd>
+#include <stdexcept>
 #include <string>
-#include <unordered_map>
+#include <utility>
 
 #include "hash_table.hpp"
 
 namespace aydogan
 {
   using Dictionary = HashTable;
-  using DictionaryStorage = std::unordered_map< std::string, Dictionary >;
+
+  class DictionaryStorage
+  {
+  public:
+    std::size_t count(const std::string & name) const
+    {
+      return contains(name) ? 1 : 0;
+    }
+
+    bool contains(const std::string & name) const
+    {
+      for (ConstIterator< Entry > it = data_.cbegin(); it != data_.cend(); ++it) {
+        if ((*it).name == name) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    Dictionary & at(const std::string & name)
+    {
+      for (Iterator< Entry > it = data_.begin(); it != data_.end(); ++it) {
+        if ((*it).name == name) {
+          return (*it).dict;
+        }
+      }
+
+      throw std::logic_error("dictionary not found");
+    }
+
+    const Dictionary & at(const std::string & name) const
+    {
+      for (ConstIterator< Entry > it = data_.cbegin(); it != data_.cend(); ++it) {
+        if ((*it).name == name) {
+          return (*it).dict;
+        }
+      }
+
+      throw std::logic_error("dictionary not found");
+    }
+
+    void emplace(const std::string & name, const Dictionary & dict)
+    {
+      if (contains(name)) {
+        throw std::logic_error("dictionary already exists");
+      }
+
+      data_.insertAfter(data_.beforeBegin(), Entry(name, dict));
+    }
+
+    void emplace(const std::string & name, Dictionary && dict)
+    {
+      if (contains(name)) {
+        throw std::logic_error("dictionary already exists");
+      }
+
+      data_.insertAfter(data_.beforeBegin(), Entry(name, std::move(dict)));
+    }
+
+    void erase(const std::string & name)
+    {
+      Iterator< Entry > prev = data_.beforeBegin();
+      Iterator< Entry > cur = data_.begin();
+
+      while (cur != data_.end()) {
+        if ((*cur).name == name) {
+          data_.eraseAfter(prev);
+          return;
+        }
+
+        ++prev;
+        ++cur;
+      }
+
+      throw std::logic_error("dictionary not found");
+    }
+
+  private:
+    struct Entry
+    {
+      Entry():
+        name(),
+        dict()
+      {}
+
+      Entry(const std::string & entryName, const Dictionary & entryDict):
+        name(entryName),
+        dict(entryDict)
+      {}
+
+      Entry(const std::string & entryName, Dictionary && entryDict):
+        name(entryName),
+        dict(std::move(entryDict))
+      {}
+
+      std::string name;
+      Dictionary dict;
+    };
+
+    List< Entry > data_;
+  };
 
   void createDict(std::istream &, std::ostream &, DictionaryStorage &);
   void showDict(std::istream &, std::ostream &, DictionaryStorage &);
